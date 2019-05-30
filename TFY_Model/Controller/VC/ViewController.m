e7a7339d9e3934de53aa2dbbba938256fa945cc5
tianfengyou <420144542@qq.com>
2019-05-30 16:30:27 +0800

    优化

--- TFY_Model/Controller/VC/ViewController.m
+++ TFY_Model/Controller/VC/ViewController.m
@@ -46,8 +46,49 @@
     /************** 指定路径只解析ResponseBody对象 **************/
     ResponseBody * body = [ResponseBody tfy_ModelWithJson:jsonString keyPath:@"ResponseBody"];
     NSLog(@"指定路径只解析ResponseBody对象 ResponseBody = %@\n\n\n",body);
+    EndorseRule *hh = [EndorseRule new];
+    hh.ruleNote = @"你好";
+    hh.ruleNote_En = @"这是一个测试数据";
+    hh.ruleRemarks = @"参数变";
+    hh.ruleRemarks_En = @"中文数据";
+    hh.ruleRestriction = @"奥会计师咖啡连锁店白费力气播放器分离器吧";
     
+    
+    [TFY_ModelSqlite removeAllModel];
+    
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        
+        [TFY_ModelSqlite insert:hh];
+        NSLog(@"线程1.存储单个模型对象到数据库演示代码");
+    });
+    
+    NSArray *person = [TFY_ModelSqlite query:[EndorseRule class]];
+    
+    [person enumerateObjectsUsingBlock:^(EndorseRule  *_Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+         NSLog(@"TFY_ModelSqlite model = %@\n\n",obj.ruleRemarks);
+    }];
+    
+    NSArray * persons = [self makeArrayPerson];
+    [TFY_ModelSqlite inserts:persons];
+    
+    NSString * path = [TFY_ModelSqlite localPathWithModel:[EndorseRule class]];
+    NSLog(@"localPath = %@",path);
 }
 
+- (NSArray *)makeArrayPerson {
+    NSMutableArray * personArray = [NSMutableArray array];
+    for (int i = 0; i < 10; i++) {
+        EndorseRule *hh = [EndorseRule new];
+        hh.ruleNote = @"你好";
+        hh.ruleNote_En = @"这是一个测试数据";
+        hh.ruleRemarks = @"参数变";
+        hh.ruleRemarks_En = @"中文数据";
+        hh.ruleRestriction = @"奥会计师咖啡连锁店白费力气播放器分离器吧";
+    
+        [personArray addObject:hh];
+    }
+    return personArray;
+}
 
 @end
