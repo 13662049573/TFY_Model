f069cce2846e3e0b087930ddc5e6ebf6dff1140f
tfyzxc13662049573 <420144542@qq.com>
2020-06-08 13:51:25 +0800

    youua

--- TFY_Model/TFY_ModelKit/TFY_ModelSqliteHeader.h
+++ TFY_Model/TFY_ModelKit/TFY_ModelSqliteHeader.h
@@ -4,7 +4,7 @@
 //
 //  Created by 田风有 on 2019/5/30.
 //  Copyright © 2019 恋机科技. All rights reserved.
-//  最新版本号: 
+//  最新版本号: 2.6.8
 
 #ifndef TFY_ModelSqliteHeader_h
 #define TFY_ModelSqliteHeader_h
