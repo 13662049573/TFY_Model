6e5604b6f6ffaf8efb6f30649f6a29179aede3bf
tfyzxc13662049573 <420144542@qq.com>
2020-06-08 13:55:34 +0800

    youhua

--- TFY_Model/TFY_ModelKit/TFY_ModelSqliteHeader.h
+++ TFY_Model/TFY_ModelKit/TFY_ModelSqliteHeader.h
@@ -9,7 +9,23 @@
 #ifndef TFY_ModelSqliteHeader_h
 #define TFY_ModelSqliteHeader_h
 
+
+
+#endif /* TFY_ModelSqliteHeader_h */
+
+#import <Foundation/Foundation.h>
+
+#if __has_include(<TFY_ModelKit/TFY_ModelSqliteHeader.h>)
+
+FOUNDATION_EXPORT double TFY_ModelVersionNumber;
+FOUNDATION_EXPORT const unsigned char TFY_ModelVersionString[];
+
+#import <TFY_ModelKit/NSObject+TFY_Model.h>
+#import <TFY_ModelKit/TFY_ModelSqlite.h>
+
+#else
+
 #import "NSObject+TFY_Model.h"
 #import "TFY_ModelSqlite.h"
 
-#endif /* TFY_ModelSqliteHeader_h */
+#endif
